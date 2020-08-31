6954d0b9961921c244aa4b840b0521f2c15c521a
Giorgio Ventura <gventuraagramonte@gmail.com>
2020-08-31 03:55:53 -0500

    course done

---
+++ README.md
@@ -0,0 +1,5 @@
+# USING THIS CODE
+
+- npm install
+- npm run dev for deploy in dev previosly config file .env with environment variables.
+- npm run start in production mode
---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const { makeExecutableSchema } = require("graphql-tools");
+const express = require("express");
+const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require("./lib/resolvers");
+
+const app = express();
+const port = process.env.port || 3000;
+const isDev = process.env.NODE_ENV.trimRight() !== "production";
+
+const typeDefs = readFileSync(
+  join(__dirname, "lib", "schema.graphql"),
+  "utf-8"
+);
+
+// definiendo el schema
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+app.use(cors());
+
+app.use(
+  "/api",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: isDev,
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
